450ff93bc9eacedbcf02e3814e7435bedb83721b
divyabharathiofficial <135594574+divyabharathiofficial@users.noreply.github.com>
2023-06-08 16:47:49 +0530

    Add files via upload

---
+++ area of the circle.py
@@ -0,0 +1,6 @@
+import math as M
+print("\t\tPROGRAM TO FIND A AREA OF THE CIRCLE")
+print("\t\t--------------------------------------")
+radius=float(input("\nENTER THE RADIUS OF THE CIRCLE : "))
+a=M.pi*radius*radius
+print("\nTHE AREA OF THE GIVEN CIRCLE WITH RADIUS",radius,"is",a)
